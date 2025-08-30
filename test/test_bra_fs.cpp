1938b78f731b44188d65ed30ec76258692c17f40
Raffaello Bertini <raffaellobertini@gmail.com>
2025-08-30 14:54:16 +0100

    code rev

--- test/test_bra_fs.cpp
+++ test/test_bra_fs.cpp
@@ -27,7 +27,6 @@ int test_bra_fs_sfx_filename_adjust()
         return format("{}{}{}", s, BRA_FILE_EXT, tmp ? BRA_SFX_TMP_FILE_EXT : BRA_SFX_FILE_EXT);
     };
 
-
     bool tmp = false;
     ASSERT_EQ(bra_fs_filename_sfx_adjust("test", tmp).string(), exp("test", tmp));
     ASSERT_EQ(bra_fs_filename_sfx_adjust(string("test") + BRA_FILE_EXT, tmp).string(), exp("test", tmp));
@@ -37,7 +36,6 @@ int test_bra_fs_sfx_filename_adjust()
     ASSERT_EQ(bra_fs_filename_sfx_adjust("test", tmp).string(), exp("test", tmp));
     ASSERT_EQ(bra_fs_filename_sfx_adjust(string("test") + BRA_FILE_EXT, tmp).string(), exp("test", tmp));
     ASSERT_EQ(bra_fs_filename_sfx_adjust(string("test") + BRA_FILE_EXT + BRA_SFX_FILE_EXT, tmp).string(), exp("test", tmp));
-
 
     return 0;
 }

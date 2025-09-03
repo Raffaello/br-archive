92ddb1f72c623d2a7a75086d21a58469d985ad03
Raffaello Bertini <raffaellobertini@gmail.com>
2025-09-03 02:03:11 +0100

    fix lib bra_printf_msg

--- src/lib_bra.c
+++ src/lib_bra.c
@@ -614,7 +614,7 @@ bool bra_io_decode_and_write_to_disk(bra_io_file_t* f)
     {
     case BRA_ATTR_FILE:
     {
-        g_msg_cb("Extracting file: %s ...", mf.name);
+        bra_printf_msg("Extracting file: %s ...", mf.name);
 
         bra_io_file_t f2;
         // NOTE: the directory must have been created in the previous file
@@ -635,7 +635,7 @@ bool bra_io_decode_and_write_to_disk(bra_io_file_t* f)
     break;
     case BRA_ATTR_DIR:
     {
-        g_msg_cb("Creating dir: %s", mf.name);
+        bra_printf_msg("Creating dir: %s", mf.name);
         if (!bra_fs_dir_make(mf.name))
             goto BRA_IO_DECODE_ERR;
 

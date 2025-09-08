58ba71236be93f524cb4eab0cf1b4ef6bfb11860
Raffaello Bertini <raffaellobertini@gmail.com>
2025-09-08 17:54:43 +0100

    code rev

--- src/bra_sfx.cpp
+++ src/bra_sfx.cpp
@@ -29,7 +29,7 @@ protected:
     {
         const fs::path p(m_argv0);
 
-        bra_log_printf("  %s       : to start un-archiving.\n", p.filename().string().c_str());
+        bra_log_printf("  %s       : extract embedded archive.\n", p.filename().string().c_str());
     };
 
     virtual void help_example() const override

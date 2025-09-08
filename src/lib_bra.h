a2e6ba4d51cb82cf7495dfdca0080425c51dc732
Raffaello Bertini <raffaellobertini@gmail.com>
2025-09-08 17:47:13 +0100

    doc code

--- src/lib_bra.h
+++ src/lib_bra.h
@@ -153,10 +153,40 @@ void bra_io_file_seek_error(bra_io_file_t* bf);
  */
 void bra_io_file_write_error(bra_io_file_t* bf);
 
+/**
+ * @brief Detect if the given filename @p fn is an ELF.
+ *
+ * @see bra_io_pe_exe
+ * @see bra_io_is_sfx
+ *
+ * @param fn
+ * @return true
+ * @return false
+ */
 bool bra_io_is_elf(const char* fn);
 
+/**
+ * @brief Detect if the given filename @p fn is a PE/EXE.
+ *
+ * @see bra_io_is_elf
+ * @see bra_io_is_sfx
+ *
+ * @param fn
+ * @return true
+ * @return false
+ */
 bool bra_io_is_pe_exe(const char* fn);
 
+/**
+ * @brief Detect if the given filename @p fn is a possible SFX archive.
+ *
+ * @see bra_io_is_elf
+ * @see bra_io_is_pe_exe
+ *
+ * @param fn
+ * @return true
+ * @return false
+ */
 bool bra_io_is_sfx(const char* fn);
 
 /**
